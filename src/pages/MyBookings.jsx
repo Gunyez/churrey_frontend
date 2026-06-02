84e36e14bfa2562f012f591adbf8edeab3dba07a
Gunyez <lagatbrian69@gmail.com>
2026-06-02 11:22:47 +0300

    Update MyBookings.jsx

--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -1,65 +1,65 @@
-import { useContext, useEffect, useState } from "react";
-import api from "../api/api";
-import "../styles/bookings.css";
-import { AuthContext } from "../context/AuthContext";
+// import { useContext, useEffect, useState } from "react";
+// import api from "../api/api";
+// import "../styles/bookings.css";
+// import { AuthContext } from "../context/AuthContext";
 
-const MyBookings = () => {
-  const [bookings, setBookings] = useState([]);
+// const MyBookings = () => {
+//   const [bookings, setBookings] = useState([]);
 
-  useEffect(() => {
-    fetchBookings();
-  }, []);
+//   useEffect(() => {
+//     fetchBookings();
+//   }, []);
 
-  const fetchBookings = async () => {
-    try {
-      const res = await api.get("/bookings/my");
-      setBookings(res.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+//   const fetchBookings = async () => {
+//     try {
+//       const res = await api.get("/bookings/my");
+//       setBookings(res.data);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
 
-  const handleCancel = async (id) => {
-    try {
-      await api.delete(`/bookings/${id}`);
-      fetchBookings();
-    } catch (err) {
-      alert("Cancel failed");
-    }
-  };
+//   const handleCancel = async (id) => {
+//     try {
+//       await api.delete(`/bookings/${id}`);
+//       fetchBookings();
+//     } catch (err) {
+//       alert("Cancel failed");
+//     }
+//   };
 
-  const { user } = useContext(AuthContext);
-  console.log(user);
+//   const { user } = useContext(AuthContext);
+//   console.log(user);
   
-  if (!user) return <Navigate to="/login" />;
+//   if (!user) return <Navigate to="/login" />;
 
-  return (
-    <div className="mybookingCard" key={b._id}>
+//   return (
+//     <div className="mybookingCard" key={b._id}>
 
-        <img src={b.houseId?.photos?.[0]} alt="" />
+//         <img src={b.houseId?.photos?.[0]} alt="" />
 
-        <div className="bookingInfo">
-            <h3>{b.houseId?.title}</h3>
+//         <div className="bookingInfo">
+//             <h3>{b.houseId?.title}</h3>
 
-            <p>
-            📅 {new Date(b.startDate).toDateString()} -{" "}
-            {new Date(b.endDate).toDateString()}
-            </p>
+//             <p>
+//             📅 {new Date(b.startDate).toDateString()} -{" "}
+//             {new Date(b.endDate).toDateString()}
+//             </p>
 
-            <p>💰 KES {b.totalPrice}</p>
-        </div>
+//             <p>💰 KES {b.totalPrice}</p>
+//         </div>
 
-        <div className="mybookingActions">
-            <button
-            className="cancelBtn"
-            onClick={() => handleCancel(b._id)}
-            >
-            Cancel Booking
-            </button>
-        </div>
+//         <div className="mybookingActions">
+//             <button
+//             className="cancelBtn"
+//             onClick={() => handleCancel(b._id)}
+//             >
+//             Cancel Booking
+//             </button>
+//         </div>
 
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export default MyBookings;
+// export default MyBookings;
